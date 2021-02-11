9940ef31e906ed6d971e7c497beab13183bd930e
Aravindhan <hello@codefry.club>
2021-02-11 10:26:12 +0530

    Welcome Screen complete

--- lib/Screens/welcome/components/body.dart
+++ lib/Screens/welcome/components/body.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../../../components/rounded_button.dart';
+import './background.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Positioned(
-            child: Image.asset(
-              'assets/images/main_top.png',
+          Text(
+            'Welcome to Healthy Being App',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
             ),
-            width: size.width * 0.4,
-          )
+          ),
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          SvgPicture.asset(
+            'assets/icons/chat.svg',
+            height: size.height * 0.4,
+          ),
+          SizedBox(
+            height: size.height * 0.05,
+          ),
+          RoundedButton(
+            text: 'Login',
+            press: () {},
+          ),
+          RoundedButton(
+            text: 'SignUp',
+            press: () {},
+            textcolor: Colors.black,
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Well Being App',
+      title: 'Healthy Being App',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
